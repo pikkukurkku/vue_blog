d364e5fef3261e71895725fb4d4f10ec6d5c2cd2
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-06-11 23:17:57 +0200

    using refs for reactivity

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,23 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    console.log(this)
+    console.log(this);
 
-    const p = ref(null)
+    const p = ref(null);
+    console.log(p, p.value);
 
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log(p);
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello, ninjas";
     };
 
     return { name, age, handleClick, p };
