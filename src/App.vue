552e86a36ed29ef9d391930bf26803020c1e19e6
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-06-13 14:23:50 +0200

    reusable composable functions

--- src/App.vue
+++ src/App.vue
@@ -11,29 +11,16 @@
 
 <script>
 import PostList from "./components/PostList.vue";
-import { ref } from "vue";
+import getPosts from "./composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
+    const { posts, error, load} = getPosts()
 
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("no data available");
-        }
-        posts.value = await data.json()
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
+    load()
 
-    load();
     return { posts, error };
   },
 };
